b71e6c99a4a7f6767b9397a5ef9082054f25ae51
TheBaconPug <48840784+TheBaconPug@users.noreply.github.com>
2021-04-18 14:59:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function invite() {
+    window.open('https://discord.com/api/oauth2/authorize?client_id=832818955634475018&permissions=2147765312&scope=bot')
+}
+
+function info() {
+    window.location.replace('./info')
+}
+
+function back() {
+    window.location.replace('./index')
+}
